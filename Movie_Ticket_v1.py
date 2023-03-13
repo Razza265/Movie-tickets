e5b6a77ffb31b071a4a6824020ce5a2e3b746fbe
Razza265 <127698055+Razza265@users.noreply.github.com>
2023-03-14 11:15:10 +1300

    update

---
+++ Movie_Ticket_v1.py
@@ -0,0 +1,19 @@
+"""Movie theatre ticketing system - v1
+welcome screen and set up variables
+created by patrick baker
+"""
+
+# component 1 - welcome screen and set up variables
+def sell_tickets():
+    print("******* Fanfair Movies - ticketing system *******\n")
+    adult_tickets = 0
+    child_tickets = 0
+    student_tickets = 0
+    gift_tickets = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+
+#main routine
+sell_tickets()
